b6ca2e12d46ff7beac6d76aeff921ee8f81b7bd0
Martin Etchebarne <martinetchebarne829@gmail.com>
2025-07-21 19:24:31 -0300

    feat: integrate @telegram-apps/sdk-react and implement test popup functionality in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
+import { useEffect } from 'react';
+import { showPopup } from '@telegram-apps/sdk-react';
+
 const App = () => {
+  useEffect(() => {
+    showPopup({
+      title: 'Hello',
+      message: 'This is a popup',
+      buttons: [
+        {
+          id: 'yes',
+          text: 'Yes',
+        },
+        {
+          id: 'no',
+          type: 'destructive',
+          text: 'No',
+        },
+      ],
+    });
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Rsbuild with React</h1>
