1e32faa648aab7aa620a5bd8bda03e2370e9f055
Martin Etchebarne <martinetchebarne829@gmail.com>
2025-07-21 18:32:10 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Rsbuild with React</h1>
+      <p className="text-lg">Start building amazing things with Rsbuild.</p>
+    </div>
+  );
+};
+
+export default App;
